e75d16c82fbde0698d02d4c2875bf261074f132b
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-17 01:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-//your code here
+let images = document.getElementById("images");
+let resetBtn = document.getElementById("reset");
+let verifyBtn = document.getElementById("verify");
+let verifyPara = document.getElementById("para");
+
+
+let randomImg = Math.floor(Math.random() * 5) + 1;
+
+images.innerHTML += `<img class="img${randomImg}"/>`
+
+let imageClicked = [];
+
+const allImages = document.querySelectorAll("#images img");
+
+allImages.forEach((img)=>{
+    img.addEventListener("click", () =>{
+        if(imageClicked.length >= 2 || img.classList.contains("selected")){
+            return;
+        }
+
+        img.classList.add("selected");
+        imageClicked.push(img);
+
+        resetBtn.style.display = "inline-block";
+
+        if(imageClicked.length === 2){
+            verifyBtn.style.display = "inline-block";
+        }else{
+            verifyBtn.style.display = "none";
+        }
+    });
+});
+
+
+verifyBtn.addEventListener("click", ()=>{
+
+    verifyPara.style.display = "block";
+    
+    if(imageClicked[0].classList[0] === imageClicked[1].classList[0]){
+        verifyPara.textContent = `You are a human. Congratulations!`;
+        verifyPara.style.color = "green";
+    }else{
+        verifyPara.innerHTML = `We can't verify you as a human. You selected the non-identical tiles.`;
+        verifyPara.style.color = "red";
+    }
+
+    verifyBtn.style.display = "none";
+});
+
+
+
+resetBtn.addEventListener("click", () =>{
+    imageClicked.forEach((img) => img.classList.remove("selected"));
+    imageClicked = [];
+    
+    resetBtn.style.display = "none";
+    verifyBtn.style.display = "none";
+    
+    verifyPara.textContent = "";
+    verifyPara.style.display = "none";
+});
